fab0ec4a9b93116c3c1ff2f56c9c3732b0c2b907
SmithNola <35985021+SmithNola@users.noreply.github.com>
2019-10-04 02:12:31 -0400

    Add files via upload
    Created title screen

--- Game.java
+++ Game.java
@@ -1,11 +1,21 @@
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Font;
 import java.util.Random;
 import java.util.Scanner;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 public class Game {
 	JFrame window;
 	Container con;
+	JPanel titleNamePanel, startButtonPanel;
+	JLabel titleName;
+	Font titleFont = new Font("Times New Roman", Font.PLAIN, 60);
+	JButton startButton;
+	Font startFont = new Font("Times New Roman", Font.PLAIN, 30);
 	
 	 public static void main(String[] args){
 		new Game();
@@ -19,12 +29,36 @@ public class Game {
 		 window.setLayout(null);
 		 window.setVisible(true);
 		 con=window.getContentPane();
+		 
+		 titleNamePanel= new JPanel();
+		 titleNamePanel.setBounds(100,100,600,150);
+		 titleNamePanel.setBackground(Color.black);//color of background
+		 titleName=new JLabel("A Knight's Quest");
+		 titleName.setForeground(Color.white);//color of text
+		 titleName.setFont(titleFont);
+		 
+		 startButtonPanel = new JPanel();
+		 startButtonPanel.setBounds(300,400,200,100);
+		 startButtonPanel.setBackground(Color.black);
+		 
+		 startButton = new JButton("START");
+		 startButton.setBackground(Color.black);
+		 startButton.setForeground(Color.white);
+		 startButton.setFont(startFont);
+		 
+		 titleNamePanel.add(titleName);
+		 startButtonPanel.add(startButton);
+		 
+		 con.add(titleNamePanel);
+		 con.add(startButtonPanel);
+		 window.setVisible(true);
 		 int roll;//to roll dice
 		 String name;//player name
 		 Scanner keyboard = new Scanner(System.in);
 		 name=keyboard.nextLine();
 		 Player p1 = new Player(); 
 		 p1.setName(name);
+		
 	 }
 	 
 	 public static int Dice (){
