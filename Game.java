2eba7329d3283a882c912ba25630325c2a34ca90
SmithNola <35985021+SmithNola@users.noreply.github.com>
2019-10-07 00:50:30 -0400

    Add files via upload

--- Game.java
+++ Game.java
@@ -1,6 +1,8 @@
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,20 +10,26 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 public class Game {
 	JFrame window;
 	Container con;
-	JPanel titleNamePanel, startButtonPanel, howToPanel;
+	JPanel titleNamePanel, startButtonPanel, howToPanel,mainTextPanel,choiceButtonPanel;
 	JLabel titleName;
 	Font titleFont = new Font("Times New Roman", Font.PLAIN, 60);
 	JButton startButton,howToButton;
-	Font buttonFont = new Font("Times New Roman", Font.PLAIN, 30);
+	Font normalFont = new Font("Times New Roman", Font.PLAIN, 30);
+	JTextArea mainTextArea;
+	
+	startScreenHandler startHandler = new startScreenHandler();
+	howToScreenHandler howToHandler = new howToScreenHandler();
 	
 	 public static void main(String[] args){
 		new Game();
 	 } 
 	 
 	 public Game(){
+		 //creates game window
 		 window = new JFrame();
 		 window.setSize(800,600);
 		 window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -44,12 +52,14 @@ public class Game {
 		 howToButton = new JButton("How To Play");
 		 howToButton.setBackground(Color.black);
 		 howToButton.setForeground(Color.white);
-		 howToButton.setFont(buttonFont);
+		 howToButton.setFont(normalFont);
+		 howToButton.addActionListener(howToHandler);
 		 
 		 startButton = new JButton("START");
 		 startButton.setBackground(Color.black);
 		 startButton.setForeground(Color.white);
-		 startButton.setFont(buttonFont);
+		 startButton.setFont(normalFont);
+		 startButton.addActionListener(startHandler);
 		 
 		 startButtonPanel = new JPanel();
 		 startButtonPanel.setBounds(300,400,150,65);
@@ -73,7 +83,65 @@ public class Game {
 		
 	 }
 	 
-	 public static int Dice (){
+	 public void createGameScreen(){
+		 
+		 titleNamePanel.setVisible(false);
+		 startButtonPanel.setVisible(false);
+		 howToPanel.setVisible(false);
+		 
+		 mainTextPanel = new JPanel();
+		 mainTextPanel.setBounds(100,100,600,250);
+		 mainTextPanel.setBackground(Color.blue);
+		 con.add(mainTextPanel);
+		 
+		 mainTextArea = new JTextArea();
+		 mainTextArea.setBounds(100,100,600,250);
+		 mainTextArea.setBackground(Color.black);
+		 mainTextArea.setForeground(Color.white);
+		 mainTextArea.setFont(normalFont);
+		 mainTextArea.setLineWrap(true);
+		 mainTextArea.setEditable(false); 
+		 mainTextPanel.add(mainTextArea);
+		 
+		 choiceButtonPanel = new JPanel();
+		 choiceButtonPanel.setBounds(250, 350, 300, 150);
+		 choiceButtonPanel.setBackground(Color.red);
+		 con.add(choiceButtonPanel);
+	 }
+	 
+	 public void howToScreen(){
+		 titleNamePanel.setVisible(false);
+		 startButtonPanel.setVisible(false);
+		 howToPanel.setVisible(false);
+		 
+		 mainTextPanel = new JPanel();
+		 mainTextPanel.setBounds(100,100,600,250);
+		 mainTextPanel.setBackground(Color.blue);
+		 con.add(mainTextPanel);
+		 
+		 mainTextArea = new JTextArea();
+		 mainTextArea.setBounds(100,100,600,250);
+		 mainTextArea.setBackground(Color.black);
+		 mainTextArea.setForeground(Color.white);
+		 mainTextArea.setFont(normalFont);
+		 mainTextArea.setLineWrap(true);
+		 mainTextArea.setEditable(false); 
+		 mainTextPanel.add(mainTextArea);
+	 }
+	 
+	 public class howToScreenHandler implements ActionListener{
+		 	public void actionPerformed(ActionEvent event){
+		 		howToScreen();
+		 	}
+}
+	 
+	 public class startScreenHandler implements ActionListener{
+			 	public void actionPerformed(ActionEvent event){
+			 		createGameScreen();
+			 	}
+	 }
+	 
+	 public static int Dice(){
 		 Random rand = new Random(); 
 		 int result = rand.nextInt(7);//will return a random integer from 0 to 6
 		 return result;
