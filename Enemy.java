09cd6cb5ee78581d76eb83a936558b91b6008dbf
SmithNola <35985021+SmithNola@users.noreply.github.com>
2020-03-04 01:00:35 -0500

    Add files via upload

--- Enemy.java
+++ Enemy.java
@@ -4,15 +4,6 @@ public class Enemy {
 	private String type;
 	private String weapon;
 	
-	public int getRand(int r){//generate random damage
-		Random rand = new Random();
-		int num = 0;
-		while(num == 0){
-			num = rand.nextInt(r+1);
-		}
-		return num;
-	}
-	
 	public void setHealth(int newHealth){
 		this.health=newHealth;
 	}
@@ -23,6 +14,21 @@ public class Enemy {
 	
 	public void setType(String newType){
 		this.type=newType;
+		
+		switch(newType){//sets the weapon and damage based on type
+			case "Bandit":
+				weapon = "Wooden Sword";
+				health = 15;
+				break;
+			case "Big Dog":
+				weapon = "Teeth";
+				health = 25;
+				break;
+			case "Ogre":
+				weapon = "Axe";
+				health = 35;
+				break;
+		}
 	}
 	
 	public String getType(){
@@ -37,20 +43,28 @@ public class Enemy {
 		return weapon;
 	}
 	
+	public int getRand(int r){//generate random number for damage
+		Random rand = new Random();
+		int num = 0;
+		while(num == 0){
+			num = rand.nextInt(r+1);
+		}
+		return num;
+	}
+	
 	public int weapond(){//sets weapon damage based on current weapon
 		int weapond = 0;
 		switch(this.weapon){
-		case "Wooden Sword":
-			weapond=getRand(5);
-			break;
-		case "Teeth":
-			weapond=getRand(13);
-			break;
-		case "Axe":
-			weapond=getRand(20);
-			break;
+			case "Wooden Sword":
+				weapond=getRand(5);
+				break;
+			case "Teeth":
+				weapond=getRand(10);
+				break;
+			case "Axe":
+				weapond=getRand(15);
+				break;
 		}
 		return weapond;
 	}
-	
 }
